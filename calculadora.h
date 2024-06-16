08d92e7b2cfeeff77cb6ab44abfd6faf7d341698
Brenda Leal <100167546+endyyxs@users.noreply.github.com>
2024-06-16 19:11:22 -0300

    Update calculadora.h

--- calculadora.h
+++ calculadora.h
@@ -1,1 +1,13 @@
+#ifndef EXPRESSAO_H
+#define EXPRESSAO_H
 
+typedef struct {
+    char posFixa[512];     // Expressão na forma pos fixa, como 3 12 4 + *
+    char inFixa[512];      // Expressão na forma pos fixa, como 3 * (12 + 4)
+    float Valor;           // Valor numérico da expressão  
+} Expressao;
+
+char *getFormaInFixa(char *Str);
+float getValor(char *Str);          // Calcula o valor de Str (na forma posFixa)
+
+#endif
